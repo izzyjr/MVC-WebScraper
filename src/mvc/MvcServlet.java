2e0c38c5d60a218e7cb7e53662ceb60a504f13ca
Israel Mesa <mesa.israel50@gmail.com>
2018-07-03 10:59:53 -0400

    Controller(Servlet)

---
+++ src/mvc/MvcServlet.java
@@ -0,0 +1,54 @@
+package mvc;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.openqa.selenium.WebElement;
+
+
+
+/**
+ * Servlet implementation class MvcServlet
+ */
+@WebServlet("/MvcServlet")
+public class MvcServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MvcServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<WebElement> tableRows = WebElementDataUtil.getWebElement();
+				
+		request.setAttribute("table_rows", tableRows);
+				
+		RequestDispatcher dispatcher = request.getRequestDispatcher("view_table_rows.jsp");
+				
+		dispatcher.forward(request, response);	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
