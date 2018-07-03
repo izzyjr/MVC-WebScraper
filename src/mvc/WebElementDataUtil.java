99c67682c9ddc7bbafb799263a06dc309f2db2a9
Israel Mesa <mesa.israel50@gmail.com>
2018-07-03 10:09:57 -0400

    Model(Helper CLass)

---
+++ src/mvc/WebElementDataUtil.java
@@ -0,0 +1,29 @@
+package mvc;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebElementDataUtil {
+	
+	public static List<WebElement> getWebElement() {
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/israelmesa/Desktop/ChromeDriver/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://finance.yahoo.com/cryptocurrencies?count=120");
+		
+		String xpath = "//*[@id=\"scr-res-table\"]/table/tbody/tr[*]";
+		
+        List<WebElement> tableRows = driver.findElements(By.xpath(xpath));
+
+        driver.quit();
+        
+        return tableRows;
+		
+	}
+
+}
